a013cb9556cd08595f84ab90b5c7b1c96fa52ae6
Rhett Beardemphl <rhettbeardemphl@hotmail.com>
2023-09-11 19:20:07 -0700

    loopback w/ first model

---
+++ ecomm-backend/src/__tests__/acceptance/test-helper.ts
@@ -0,0 +1,32 @@
+import {EcommBackendApplication} from '../..';
+import {
+  createRestAppClient,
+  givenHttpServerConfig,
+  Client,
+} from '@loopback/testlab';
+
+export async function setupApplication(): Promise<AppWithClient> {
+  const restConfig = givenHttpServerConfig({
+    // Customize the server configuration here.
+    // Empty values (undefined, '') will be ignored by the helper.
+    //
+    // host: process.env.HOST,
+    // port: +process.env.PORT,
+  });
+
+  const app = new EcommBackendApplication({
+    rest: restConfig,
+  });
+
+  await app.boot();
+  await app.start();
+
+  const client = createRestAppClient(app);
+
+  return {app, client};
+}
+
+export interface AppWithClient {
+  app: EcommBackendApplication;
+  client: Client;
+}
---
+++ ecomm-backend/src/models/orders.model.ts
@@ -0,0 +1,41 @@
+import {Entity, model, property} from '@loopback/repository';
+
+@model()
+export class Orders extends Entity {
+  @property({
+    type: 'array',
+    itemType: 'object',
+    required: true,
+  })
+  order: object[];
+
+  @property({
+    type: 'string',
+    required: true,
+  })
+  userId: string;
+
+  @property({
+    type: 'string',
+    id: true,
+    generated: true,
+  })
+  id?: string;
+
+  @property({
+    type: 'string',
+    required: true,
+  })
+  status: string;
+
+
+  constructor(data?: Partial<Orders>) {
+    super(data);
+  }
+}
+
+export interface OrdersRelations {
+  // describe navigational properties here
+}
+
+export type OrdersWithRelations = Orders & OrdersRelations;
