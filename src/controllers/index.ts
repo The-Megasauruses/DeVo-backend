be368eebbed7787c87bb80db0e61e3ae598d5f4f
Rhett Beardemphl <rhettbeardemphl@hotmail.com>
2023-09-14 10:44:24 -0700

    connected users to orders and orders to users

--- src/controllers/index.ts
+++ src/controllers/index.ts
@@ -2,3 +2,5 @@ export * from './ping.controller';
 export * from './orders.controller';
 export * from './users.controller';
 export * from './products.controller';
+export * from './orders-users.controller';
+export * from './users-orders.controller';
--- src/repositories/orders.repository.ts
+++ src/repositories/orders.repository.ts
@@ -1,16 +1,22 @@
-import {inject} from '@loopback/core';
-import {DefaultCrudRepository} from '@loopback/repository';
+import {inject, Getter} from '@loopback/core';
+import {DefaultCrudRepository, repository, BelongsToAccessor} from '@loopback/repository';
 import {DbDataSource} from '../datasources';
-import {Orders, OrdersRelations} from '../models';
+import {Orders, OrdersRelations, Users} from '../models';
+import {UsersRepository} from './users.repository';
 
 export class OrdersRepository extends DefaultCrudRepository<
   Orders,
   typeof Orders.prototype.id,
   OrdersRelations
 > {
+
+  public readonly user: BelongsToAccessor<Users, typeof Orders.prototype.id>;
+
   constructor(
-    @inject('datasources.db') dataSource: DbDataSource,
+    @inject('datasources.db') dataSource: DbDataSource, @repository.getter('UsersRepository') protected usersRepositoryGetter: Getter<UsersRepository>,
   ) {
     super(Orders, dataSource);
+    this.user = this.createBelongsToAccessorFor('user', usersRepositoryGetter,);
+    this.registerInclusionResolver('user', this.user.inclusionResolver);
   }
 }
