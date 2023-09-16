2592657ae2e3cda4b9c3f33674af0b7e1897abd4
rpruazol <rpruazol@gmail.com>
2023-09-16 18:14:04 -0400

    added users model back in

--- src/repositories/index.ts
+++ src/repositories/index.ts
@@ -2,3 +2,4 @@ export * from './orders.repository';
 export * from './products.repository';
 export * from './user-credentials.repository';
 export * from './user.repository';
+export * from './users.repository';
