93f095328494289b18b28fe3b7a896040ddc8ebe
Lacey Washington <lacey.m.washington@gmail.com>
2023-09-19 15:53:30 -0500

    got ordercontroller test to pass

--- src/__tests__/controller.test/orders.controller.test.ts
+++ src/__tests__/controller.test/orders.controller.test.ts
@@ -5,29 +5,34 @@ import {OrdersController} from '../../controllers/index'; // Import your OrdersC
 import {OrdersRepository} from '../../repositories/index'; // Import your OrdersRepository
 import {createTestDataSource} from '../test-datasource.config';
 
-const testDataSource = createTestDataSource();
-
 
 const createdOrder = {
   order: [
-    {product: 'Product A', quantity: 2, price: 10.99},
-    {product: 'Product B', quantity: 1, price: 5.99},
+    { product: 'Product A', quantity: 2, price: 10.99 },
+    { product: 'Product B', quantity: 1, price: 5.99 },
   ],
   userId: '1',
   status: 'Pending',
 };
+let app: RestApplication;
+let client: Client;
 
 describe('OrdersController', () => {
-  let app: RestApplication;
-  let client: Client;
 
   beforeAll(async () => {
     app = new RestApplication();
-    // Initialize and bind your controller and repository
-    app.controller(OrdersController); // Use the controller class directly
-    app.bind('repositories.OrdersRepository').toClass(OrdersRepository); // Bind the repository class
 
-    // Bind the application
+
+    const testDataSource = createTestDataSource();
+    app.bind('datasources.postgresql').to(testDataSource);
+
+
+    app.bind('repositories.OrdersRepository').toClass(OrdersRepository);
+
+
+    app.controller(OrdersController);
+
+
     app.bind(RestBindings.Http.CONTEXT).toDynamicValue(() => {
       return new RestServer(app, {
         openApiSpec: {
@@ -40,12 +45,8 @@ describe('OrdersController', () => {
     client = supertest.agent(app.restServer.url);
   });
 
-  afterAll(async () => {
-    await app.stop();
-  });
 
   it('should create a new order', async () => {
-
     const orderData = createdOrder;
 
     const response = await client.post('/orders').send(orderData).expect(200);
@@ -54,40 +55,39 @@ describe('OrdersController', () => {
   });
 
   it('should retrieve an order by ID', async () => {
-
     const response = await client.get(`/orders/1`).expect(200);
 
     expect(response.body).to.containEql(createdOrder);
   });
 
   it('should update an order', async () => {
-    // Create an order first (you can use the same logic as above)
-
     const updatedOrderData = {
       order: [
-        {product: 'Product A', quantity: 2, price: 10.99},
-        {product: 'Product B', quantity: 1, price: 40.00},
+        { product: 'Product A', quantity: 2, price: 10.99 },
+        { product: 'Product B', quantity: 1, price: 40.00 },
       ],
       userId: '1',
       status: 'complete',
     };
 
     await client
-      .put(`/orders/1`)
-      .send(updatedOrderData)
-      .expect(204);
+    .put(`/orders/1`)
+    .send(updatedOrderData)
+    .expect(204);
 
-    // Fetch the order again and validate the updates
     const response = await client.get(`/orders/1`).expect(200);
 
     expect(response.body).to.containEql(updatedOrderData);
   });
 
   it('should delete an order', async () => {
-
     await client.delete(`/orders/1`).expect(204);
 
     // Attempt to fetch the deleted order and ensure it's not found (404)
     await client.get(`/orders/1`).expect(404);
+  });
+
+  afterAll(async () => {
+    await app.stop();
   });
 });
